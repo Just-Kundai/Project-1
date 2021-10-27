31beef26079cafde3459747e076cd67c6b71d8f9
Just-Kundai <kundaikhuleya@outlook.com>
2021-10-27 17:10:25 +1100

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,23 @@
 Rails.application.routes.draw do
+ devise_for :accounts
+
+ root to:"public#home"
+
+ get "u/:username" => "user#profile", as: :profile
+
   root :to => "chat#index"
   resources :subjects
-  get 'chat/index'
-  get 'chat/show'
-  get 'chat/edit'
-  get 'chat/new'
+  
 
   root :to => "subject#index"
   resources :subjects
-  get 'subject/index'
-  get 'subject/show'
-  get 'subject/edit'
-  get 'subject/new'
+
 
   root :to => "user#index"
   resources :users
-  get 'user/index'
-  get 'user/show'
-  get 'user/edit'
-  get 'user/new'
 
   root :to => "tutors#index"
   resources :tutors
-  get 'tutors/index'
-  get 'tutors/show'
-  get 'tutors/edit'
-  get 'tutors/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
